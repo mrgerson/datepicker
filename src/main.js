8ed3f9ff012a01e0dbd15375a510c617e9c26522
Gerson <gedacaba@gmail.com>
2022-03-07 21:03:02 -0500

    agregando date-picker

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/es'
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {DatePicker, TimeSelect, TimePicker} from 'element-ui';
+
+Vue.use(ElementUI, { locale })
+Vue.use(ElementUI);
+Vue.use(DatePicker);
+Vue.use(TimeSelect);
+Vue.use(TimePicker);
 
 Vue.config.productionTip = false
 
